881a661d977b3323d27ca51a42ac3d0c9464283e
Leandro <40226937+LeandroReis1997@users.noreply.github.com>
2020-12-02 01:03:43 -0300

    Adicionando tela de Detalhes do produto

--- BlueModas.Web.Core/BlueModas.Web.Core/Controllers/WelcomeController.cs
+++ BlueModas.Web.Core/BlueModas.Web.Core/Controllers/WelcomeController.cs
@@ -4,8 +4,7 @@ using BlueModas.Web.Core.ViewModels;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using AutoMapper;
-
-
+using System;
 
 namespace BlueModas.Web.Core.Controllers
 {
@@ -18,12 +17,22 @@ namespace BlueModas.Web.Core.Controllers
             _mapper = mapper;
         }
 
-       
+        [Route("~/")]
+        [Route("index")]
         public async Task<IActionResult> Index()
         {
             var productService = new ProductService();
             var products = _mapper.Map<IEnumerable<ProductViewModel>>(await productService.GetAllProduct());
             return View(products);
+        }
+
+        [HttpGet]
+        [Route("viewproduct/{id}")]
+        public async Task<IActionResult> ViewProduct(Guid id)
+        {
+            var productService = new ProductService();
+            var product = _mapper.Map<ProductViewModel>(await productService.GetByProductId(id));
+            return View("ViewProduct", product);
         }
     }
 }
